409b9a1dde925b86c0efba90ebd26ca8af044866
Leandro <ldzsmaga@gmail.com>
2024-01-09 14:50:43 -0300

    feat: getUsersRepository gets user from mongo

--- repositories/get-users/mongo-get-users.ts
+++ repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
+import { MongoClient } from "../../database/mongo";
 import { IGetUsersRepository } from "../../src/controllers/get-users/protocols";
 import { User } from "../../src/models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "José",
-        lastName: "Mariano",
-        email: "Jose@ok.com",
-        password: "123",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
